101cff59586bcee83b741867e7ee65894eb0c792
klashenriksson <6956114+klashenriksson@users.noreply.github.com>
2023-12-27 17:50:14 +0100

    nice stuff

--- lua/remap.lua
+++ lua/remap.lua
@@ -1,2 +1,6 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+vim.keymap.set("n", "<leader>ft", ":FloatermNew --name=terminal --height=0.8 --width=0.7 --autoclose=2 zsh <CR>")
+vim.keymap.set("n", "t", ":FloatermToggle terminal<CR>")
+vim.keymap.set("t", "<Esc>", "<C-\\><C-n>:q<CR>")
