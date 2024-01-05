cee2f1377d700a06f60ca7e627a539a967dcfb14
klashenriksson <6956114+klashenriksson@users.noreply.github.com>
2024-01-05 09:28:41 +0100

    cool keybinds

--- lua/remap.lua
+++ lua/remap.lua
@@ -2,4 +2,6 @@ vim.g.mapleader = " "
 
 vim.keymap.set("n", "<leader>ft", ":FloatermNew --name=terminal --height=0.8 --width=0.7 --autoclose=2 zsh <CR>")
 vim.keymap.set("n", "t", ":FloatermToggle terminal<CR>")
+vim.keymap.set("n", "<leader>fn", ":FloatermNext<CR>")
+vim.keymap.set("n", "<leader>fp", ":FloatermPrev<CR>")
 vim.keymap.set("t", "<Esc>", "<C-\\><C-n>:q<CR>")
